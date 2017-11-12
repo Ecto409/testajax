fd12de33b6c43f7fae52a45a4122f5dcfbbe143a
Votre nom ou pseudoEcto409 <sgoffin1@hotmail.com>
2017-11-12 18:34:38 +0100

    test

---
+++ js.js
@@ -0,0 +1,55 @@
+// Solution 1: $.ajax() and jsonp:
+$.ajax({
+  //  type: "GET",
+  //  crossDomain: true,
+  dataType: "jsonp", // jsonp
+  url: 'http://wikidata.org/w/api.php?action=wbgetentities&sites=frwiki&titles=France&languages=zh-hans|zh-hant|fr&props=sitelinks|labels|aliases|descriptions&format=json',
+  //  data: data,
+  //  success: 1,
+}).done(function(data) {
+  var items = [];
+
+  $.each(data.entities.Q142.labels.fr, function(key, val) {
+    items.push('<li id="' + key + '1">Test 1:' + val + '</li>');
+  });
+
+  $('<ul/>', {
+    'class': 'my-new-list',
+    html: items.join('')
+  }).appendTo('#code1');
+});
+
+// Solution 2: $.ajax(), json, &calback=?:
+$.ajax({
+  //  type: "GET",
+  //  crossDomain: true,
+  dataType: "json", // json
+  url: 'http://wikidata.org/w/api.php?action=wbgetentities&sites=frwiki&titles=France&languages=zh-hans|zh-hant|fr&props=sitelinks|labels|aliases|descriptions&format=json&callback=?', // '&callback=?
+  //  data: data,
+  //  success: 1,
+}).done(function(data) {
+  var items = [];
+
+  $.each(data.entities.Q142.labels.fr, function(key, val) {
+    items.push('<li id="' + key + '2">Test 2:' + val + '</li>');
+  });
+
+  $('<ul/>', {
+    'class': 'my-new-list',
+    html: items.join('')
+  }).appendTo('#code2');
+});
+
+// Solution 3: $.getJSON() & calback=?:
+$.getJSON('http://wikidata.org/w/api.php?action=wbgetentities&sites=frwiki&titles=France&languages=zh-hans|zh-hant|fr&props=sitelinks|labels|aliases|descriptions&format=json&callback=?', function(data) {
+  var items = [];
+
+  $.each(data.entities.Q142.labels.fr, function(key, val) {
+    items.push('<li id="' + key + '3">Test 3: ' + val + '</li>');
+  });
+
+  $('<ul/>', {
+    'class': 'my-new-list',
+    html: items.join('')
+  }).appendTo('#code3');
+});
